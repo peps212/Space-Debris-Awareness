4377581ec4d871c023d6a8abbb44888c51e8302f
Peter Hague <pete.hague@gmail.com>
2023-07-29 19:38:32 +0100

    Endpoint implemented, but buggy

--- orbitserver.py
+++ orbitserver.py
@@ -11,9 +11,6 @@ import json
 import pandas as pd
 import numpy as np
 
-Earth_mu = 3.986004418e14
-debris_data = pd.read_csv("cosmos1408.csv")
-
 def meanmotion_to_semimajoraxis(mm, mu):
     return mu**(1/3)/((mm*2*np.pi/84600)**(2/3))
 
@@ -22,8 +19,33 @@ def meananomaly_to_trueanomaly(ma, e):
     return 2*np.arctan(np.sqrt((1+e)/(1-e))*np.tan(ma/2))
 
 
+def find_nodes(orbit0, orbit1,direction=1):
+    '''
+        Finds the nodes of two orbits
+        Not always correct, but good enough for now
+    '''
+    i = orbit0["INCLINATION"]
+    argperi = orbit0["ARG_OF_PERICENTER"]
+    rx, ry, rz =  np.cos(argperi)*np.cos(i), np.sin(argperi)*np.cos(i), np.sin(i)
+    i = orbit1["INCLINATION"]
+    argperi = orbit1["ARG_OF_PERICENTER"]
+    x, y, z = np.cos(argperi)*np.cos(i), np.sin(argperi)*np.cos(i), np.sin(i)
+    # cross product with reference vector
+    x, y, z = np.cross([rx, ry, rz], [x, y, z])
+    x, y, z = x*direction, y*direction, z*direction
+    # find angle with periapsis vector
+    px, py, pz = np.cos(argperi), np.sin(argperi), 0
+    ta = np.arccos(np.dot([x, y, z], [px, py, pz])/(np.linalg.norm([x, y, z])*np.linalg.norm([px, py, pz])))
+    # get radius from true anomaly
+    r = orbit1["a"]*(1-orbit1["ECCENTRICITY"]**2)/(1+orbit1["ECCENTRICITY"]*np.cos(ta))
+    # convert to cartesian
+    x, y, z = r*np.cos(ta), r*np.sin(ta), 0  
+    return x, y, z
+
+
 class MainHandler(RequestHandler):
     def get(self):
+        global debris_data
         # Get the orbital elements
         params = {}
         for param in ["a", "e", "i", "raan", "aop", "ta"]:
@@ -33,19 +55,43 @@ class MainHandler(RequestHandler):
                 self.write(json.dumps({"error": "Missing orbital elements"}))
                 return
             params[param] = float(argument)
+        params["INCLINATION"] = params["i"]
+        params["ECCENTRICITY"] = params["e"]
+        params["ARG_OF_PERICENTER"] = params["aop"]
+        for _, row in debris_data.iterrows():
+                nodes = []
+                for direction in [-1,1]:
+                    x,y,z = find_nodes(row, params, direction)
+                    nodes.append([x,y,z])
+                    x,y,z = find_nodes(params, row, direction)
+                    nodes.append([x,y,z])
+                # find the closest nodes
+                mindist = 1e12
+                for n0 in nodes:
+                    for n1 in nodes:
+                        if n0 == n1:
+                            continue
+                        dist = np.linalg.norm(np.array(n0)-np.array(n1))
+                        if dist<mindist:
+                            mindist = dist
+                debris_data["dist"] = mindist
+        sorted_data = debris_data.sort_values(by="dist")
         result = {}
-        for n, row in debris_data[1:5].iterrows():
+        for n, row in sorted_data[:5].iterrows():
             result[n] = {"semimajoraxis": meanmotion_to_semimajoraxis(row["MEAN_MOTION"], Earth_mu),
-                         "eccentricity": row["ECCENTRICITY"],
-                         "inclination": row["INCLINATION"],
-                         "raan": row["RA_OF_ASC_NODE"],
-                         "aop": row["ARG_OF_PERICENTER"],
-                         "ta": meananomaly_to_trueanomaly(row["MEAN_ANOMALY"], row["ECCENTRICITY"])}
+                            "eccentricity": row["ECCENTRICITY"],
+                            "inclination": row["INCLINATION"],
+                            "raan": row["RA_OF_ASC_NODE"],
+                            "aop": row["ARG_OF_PERICENTER"],
+                            "ta": meananomaly_to_trueanomaly(row["MEAN_ANOMALY"], row["ECCENTRICITY"])}
         self.set_status(200)
         self.write(json.dumps(result))
 
 
 if __name__ == "__main__":
+    Earth_mu = 3.986004418e14
+    debris_data = pd.read_csv("https://celestrak.org/NORAD/elements/gp.php?GROUP=1982-092&FORMAT=csv")
+    debris_data["a"] = meanmotion_to_semimajoraxis(debris_data["MEAN_MOTION"], Earth_mu)
     app = Application([
         (r"/", MainHandler),
     ])
